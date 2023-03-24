405005a50cd231613b4546686d10ec39ba9ea5c9
BreadSoup <79678268+BreadSoup@users.noreply.github.com>
2023-03-24 15:50:02 -0400

    First Commit
    Adding all the files to main

---
+++ melon/melon/MelonLoaderMod.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+using SLZ.VRMK;
+using BoneLib;
+using UnityEngine;
+using System;
+using Il2CppSystem;
+
+namespace melon
+{
+    public static class BuildInfo
+    {
+        public const string Name = "melon"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = null; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class Melon : MelonMod
+    {
+         public override void OnUpdate()
+        {
+            // string avatarName won't get send in as it isn't part of ML, you need to get it yourself
+            // (probably though the getcurrentavatar method)
+            Avatar avatar = Player.GetCurrentAvatar();
+            if (BoneLib.HelperMethods.GetCleanObjectName(avatar.name) == "char_tall")
+            {
+                LoggerInstance.Msg(avatar.name);
+                Player.rigManager.SwitchAvatar();
+            }
+        }
+    }
+}
---
+++ melon/melon/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(melon.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(melon.BuildInfo.Company)]
+[assembly: AssemblyProduct(melon.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + melon.BuildInfo.Author)]
+[assembly: AssemblyTrademark(melon.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(melon.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(melon.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(melon.Melon), melon.BuildInfo.Name, melon.BuildInfo.Version, melon.BuildInfo.Author, melon.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
